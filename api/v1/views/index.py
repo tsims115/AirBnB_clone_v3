f1239bf40a3b6f52d42cea83a2fceff5563c595e
AJS9091 <ashtonjamess@live.com>
2022-03-26 10:21:23 -0700

    adding get method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,9 @@
 """is the index for app api"""
 from api.v1.views import app_views
 from flask import jsonify
-@app_views.route('/status', strict_slashes=False)
+
+
+@app_views.route('/status', strict_slashes=False, methods=['GET'])
 def status():
+    """ returns dictionary containing status """
     return jsonify({'status': "OK"})
