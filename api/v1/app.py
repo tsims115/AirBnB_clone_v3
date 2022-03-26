0105ab9406d0b2884d6a7c9e8afa9156cbc0201a
AJS9091 <ashtonjamess@live.com>
2022-03-26 10:28:51 -0700

    pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,14 +8,16 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
-        """closes the storage on teardown"""
-        storage.close()
+    """closes the storage on teardown"""
+    storage.close()
+
 
 if __name__ == '__main__':
-    host='0.0.0.0'
-    port='5000'
+    host = "0.0.0.0"
+    port = '5000'
     if getenv("HBNB_API_HOST"):
         host = getenv("HBNB_API_HOST")
     if getenv("HBNB_API_PORT"):
