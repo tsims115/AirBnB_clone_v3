b7768647e5dfa590364524d3a016384f74ab7f4b
AJS9091 <ashtonjamess@live.com>
2022-03-26 09:45:26 -0700

    create an app before you invoke it

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,8 +5,8 @@ from models import storage
 from api.v1.views import app_views
 
 
-app.register_blueprint(app_views)
 app = Flask(__name__)
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown_db(exception):
