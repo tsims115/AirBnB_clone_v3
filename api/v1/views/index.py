5860bd852e2766d79d46f3955a226e64e95467d6
AJS9091 <ashtonjamess@live.com>
2022-03-26 17:35:47 -0700

    add task 5; passes local

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,31 @@
 """is the index for app api"""
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', strict_slashes=False, methods=['GET'])
 def status():
     """ returns dictionary containing status """
     return jsonify({'status': "OK"})
+
+@app_views.route('/stats', strict_slashes=False, methods=['GET'])
+def stats():
+    """ returns dictionary with classes and their count"""
+    classes = {
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User
+    }
+    countDict = {}
+    for objType, cls in classes.items():
+        countDict.update({objType: storage.count(cls)})
+    return jsonify(countDict)
