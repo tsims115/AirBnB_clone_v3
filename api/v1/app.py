e38b369d2a84a925619ef7dcf05a7f468c13fec1
AJS9091 <ashtonjamess@live.com>
2022-03-28 12:43:35 -0700

    cpycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -17,10 +18,13 @@ def teardown_db(exception):
 
 @app.errorhandler(404)
 def invalid_route(e):
+    """404 not found default error"""
     return jsonify({'error': 'Not found'}), 404
+
 
 @app.errorhandler(400)
 def invalid_route(e):
+    """400 not a JSON default error"""
     return jsonify({'error': 'Not a JSON'}), 400
 
 
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
+"""sets up our api for state objs"""
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
 from models.state import State
 
+
 @app_views.route('/states', strict_slashes=False, methods=['GET'])
 def states():
+    """returns all state objs"""
     stateList = list(storage.all(State).values())
     newList = []
     for state in stateList:
@@ -13,16 +16,21 @@ def states():
         newList.append(state)
     return jsonify(newList)
 
+
 @app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET'])
 def singleState(state_id=None):
+    """ returns specified state obj"""
     allStates = storage.all()
     if 'State.' + state_id in allStates:
         return jsonify(allStates['State.' + state_id].to_dict())
     else:
         abort(404)
 
-@app_views.route('/states/<state_id>', strict_slashes=False, methods=['DELETE'])
+
+@app_views.route('/states/<state_id>', strict_slashes=False,
+                 methods=['DELETE'])
 def deleteState(state_id=None):
+    """deletes specified state obj"""
     allStates = storage.all(State)
     if 'State.' + state_id in allStates:
         storage.delete(allStates['State.' + state_id])
@@ -31,8 +39,10 @@ def deleteState(state_id=None):
     else:
         abort(404)
 
+
 @app_views.route('/states', strict_slashes=False, methods=['POST'])
 def postState():
+    """creates new state objs"""
     newData = request.get_json()
     if 'name' in newData.keys():
         newState = State(**newData)
@@ -41,8 +51,11 @@ def postState():
     else:
         return jsonify({'error': 'Missing name'}), 400
 
-@app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET', 'PUT'])
+
+@app_views.route('/states/<state_id>', strict_slashes=False,
+                 methods=['GET', 'PUT'])
 def putState(state_id=None):
+    """updates existing specified state obj"""
     allStates = storage.all(State)
     newData = request.get_json()
     if 'State.' + state_id in allStates:
