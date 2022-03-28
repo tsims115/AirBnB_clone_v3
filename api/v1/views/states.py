544844bf42beb26e57a5f4d69ff259494c0fdc12
AJS9091 <ashtonjamess@live.com>
2022-03-28 13:43:39 -0700

    pycodestyle x2

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -49,7 +49,7 @@ def postState():
         newState.save()
         return jsonify(newState.to_dict()), 201
     else:
-        abort(400, {"error":"Missing name"})
+        abort(400, {"error": "Missing name"})
 
 
 @app_views.route('/states/<state_id>', strict_slashes=False,
