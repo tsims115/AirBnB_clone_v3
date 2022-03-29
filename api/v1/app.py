4f6c165aef5a96cc6fbcfaacaa88f12058f4b04b
tsims115 <tsims115@yahoo.com>
2022-03-29 12:08:19 -0700

    Fixing pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,14 +24,6 @@ def invalid_route(e):
     return jsonify({'error': 'Not found'}), 404
 
 
-"""@app.errorhandler(400)
-def invalid_route(e):
-    if type(e.description) is dict:
-        return jsonify(e.description), 400
-    else:
-        return jsonify({'error': 'Not a JSON'}), 400
-"""
-
 if __name__ == '__main__':
     host = "0.0.0.0"
     port = '5000'
